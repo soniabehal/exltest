527cdf8569ad41c8117f22a9e7dc8d452f3b2cf7
Recko <sonia@recko.io>
2019-04-24 14:20:41 +0530

    logs added while api request fetch

--- controllers/courses.js
+++ controllers/courses.js
@@ -7,13 +7,18 @@ module.exports = {
     try {
       let { body } = await requestServer(query.listCoursesQuery);
       if (!body.data) throw body.errors;
+      console.log("course id request successfull ",body);
       const courseId = body.data.listCourses.edges[0].node.id;
+      console.log("course id fetch successfull ",courseId);
 
       let units = await requestServer(query.listCardsQuery(courseId));
       if (!units.body.data) throw units.body.errors;
       let cardIds = retrieveIDs(units.body.data.listUnits.edges);
+      console.log("card ids fetch successfull");
       cardIds=cardIds.map(cd=>`(${JSON.stringify(cd)})`);
+      console.log("card ids parse successfull");
       let rows = await sqlquery(`INSERT INTO data (id) VALUES ${cardIds.join(',')}`);
+      console.log("db insert successfull");
       res.send({
         success: "true",
         message: "MySql write Successfull "+rows.message
